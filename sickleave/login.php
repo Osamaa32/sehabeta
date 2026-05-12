d4be6b5e8233b34f1b5722199e570f6ad8c1b87b
Osamaa32 <thomashey725@gmail.com>
2026-05-12 07:05:43 +0300

    Update login.php

--- sickleave/login.php
+++ sickleave/login.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['initiated'])) {
 // بيانات الاتصال بقاعدة البيانات
 $db_host = 'mysql.railway.internal';
 $db_user = 'root';
-$db_pass = 'ExvKbuJnGIvDATyXWCHtpjOFluFAgeqQ';
+$db_pass = 'vDUncyqSFYnHULjIOHYltRvPXtbLVIIl';
 $db_name = 'railway';
 $db_port = 3306;
 
